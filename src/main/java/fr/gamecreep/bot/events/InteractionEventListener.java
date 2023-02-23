88928a2cc6929f579c65f55cdd22883cdf625010
iGameCreep <gamecreepbs@gmail.com>
2023-02-23 21:59:57 +0100

    Added rolereact with buttons

--- src/main/java/fr/gamecreep/bot/events/InteractionEventListener.java
+++ src/main/java/fr/gamecreep/bot/events/InteractionEventListener.java
@@ -1,16 +1,12 @@
 package fr.gamecreep.bot.events;
 
 import fr.gamecreep.bot.commands.*;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.UserSnowflake;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import java.util.Collection;
-import java.util.Date;
-import java.util.Locale;
 
 public class InteractionEventListener extends ListenerAdapter {
     @Override
@@ -35,4 +31,50 @@ public class InteractionEventListener extends ListenerAdapter {
         }
 
     }
+
+    @Override
+    public void onButtonInteraction(ButtonInteractionEvent event) {
+        super.onButtonInteraction(event);
+
+        String twitchroleid = "1075765623830884362";
+        String youtuberoleid = "1076927052634140772";
+        String tiktokroleid = "1076926853098508399";
+
+        Role twitchrole = event.getGuild().getRoleById(twitchroleid);
+        Role youtuberole = event.getGuild().getRoleById(youtuberoleid);
+        Role tiktokrole = event.getGuild().getRoleById(tiktokroleid);
+
+        switch (event.getInteraction().getComponentId()) {
+            case "notif-twitch":
+                Role role1 = hasrole(twitchroleid, event.getMember());
+                giveRole(event, role1, twitchrole);
+                break;
+            case "notif-youtube":
+                Role role2 = hasrole(youtuberoleid, event.getMember());
+                giveRole(event, role2, youtuberole);
+                break;
+            case "notif-tiktok":
+                Role role3 = hasrole(tiktokroleid, event.getMember());
+                giveRole(event, role3, tiktokrole);
+                break;
+        }
+
+    }
+
+    public Role hasrole(String roleid, Member member) {
+        Role role = member.getRoles().stream().filter(r -> r.getId().equals(roleid)).findFirst().orElse(null);
+        if (role == null) return null;
+        else return role;
+    }
+
+    public void giveRole(ButtonInteractionEvent event, Role role, Role roleselected) {
+        if (role == null) {
+            event.getGuild().addRoleToMember(event.getMember(), roleselected).queue();
+            event.reply(String.format("Le rôle <@&%s> t'as été donné avec succès !", roleselected.getId())).setEphemeral(true).queue();
+        } else {
+            event.getGuild().removeRoleFromMember(event.getMember(), role).queue();
+            event.reply(String.format("Le rôle <@&%s> t'as été enlevé avec succès !", role.getId())).setEphemeral(true).queue();
+        }
+    }
+
 }
--- src/main/java/fr/gamecreep/bot/events/MessageEventListener.java
+++ src/main/java/fr/gamecreep/bot/events/MessageEventListener.java
@@ -1,10 +1,17 @@
 package fr.gamecreep.bot.events;
 
-import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.entities.emoji.EmojiUnion;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.EventListener;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
+import net.dv8tion.jda.api.utils.data.DataObject;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
 
 public class MessageEventListener extends ListenerAdapter {
     @Override
@@ -14,5 +21,24 @@ public class MessageEventListener extends ListenerAdapter {
         if (event.getAuthor().isBot()) return;
 
         System.out.println(String.format("[ %s > %s ] >> %s : %s", event.getGuild().getName(), event.getChannel().getName(), event.getAuthor().getAsTag(), event.getMessage().getContentDisplay()));
+
+        System.out.println(event.getMessage().getContentRaw().equals("!rolereact"));
+
+        if (event.getMessage().getContentDisplay().equals("!rolereact") && event.getAuthor().getId().equals("696753471650660412")){
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setColor(new Color(44,175,255))
+                    .setTitle("**Choix des rôles**")
+                    .setDescription("Afin d'éviter de faire des tag everyone et here sur le discord, vous pouvez choisir vos rôles pour recevoir les notifications qui vous intéressent.\n" +
+                            "Vous pouvez d'ailleurs ajouter et retirer votre rôle à tout moment.")
+                    .addField("<:twitch:1076929000330498058>", "Les notifications Twitch", true)
+                    .addField("<:youtube:1076929043154346047>", "Les notifications YouTube", true)
+                    .addField("<:tiktok:1076929073516912733>", "Les notifications TikTok", true);
+            Button twitchbtn = Button.primary("notif-twitch", Emoji.fromCustom(":twitch:", Long.parseLong("1076929000330498058"), false));
+            Button youtubebtn = Button.primary("notif-youtube", Emoji.fromCustom(":youtube:", Long.parseLong("1076929043154346047"), false));
+            Button tiktokbtn = Button.primary("notif-tiktok", Emoji.fromCustom(":tiktok:", Long.parseLong("1076929073516912733"), false));
+
+            event.getChannel().sendMessageEmbeds(embed.build()).addActionRow(twitchbtn, youtubebtn, tiktokbtn).queue();
+            event.getMessage().delete().queue();
+        }
     }
 }
